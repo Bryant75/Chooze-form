2044ea4fb714e5672b287b3313056a95a556d4a7
Bryant-Kaman <134620830+Bryant75@users.noreply.github.com>
2023-06-07 12:36:25 +0200

    End "Resultats" pages

--- index.js
+++ index.js
@@ -1,65 +1,43 @@
-let score = 0;
+let width = 16.66; // Initialise la barre de progression à 1/3
 let currentForm = 1;
-const totalForms = 3;
+function move() {
+    
+    // Cacher le formulaire actuel
+    document.getElementById(`form-${currentForm}`).classList.add('hidden');
 
-function checkAnswer(button) {
-  const selectedButton = button;
-  const options = selectedButton.parentNode.getElementsByClassName('options')[0].getElementsByTagName('button');
-  
-  for (let i = 0; i < options.length; i++) {
-    options[i].disabled = true; // désactive tous les boutons d'options
-  }
-  
-  if (selectedButton.textContent === 'Célibataire' || // Vérifiez ici les réponses correctes pour chaque question
-      selectedButton.textContent === '18-25' ||
-      selectedButton.textContent === '0') {
-    selectedButton.classList.add('correct'); // ajoute la classe "correct" pour mettre en évidence la réponse correcte
-    score++;
-  } else {
-    selectedButton.classList.add('incorrect'); // ajoute la classe "incorrect" pour mettre en évidence la réponse incorrecte
-  }
-  
-  document.getElementById('score').textContent = score; // met à jour le score affiché
-  
-  // Vérifie si toutes les questions ont été répondues
-  const allOptions = document.getElementById(`form-${currentForm}`).getElementsByClassName('options');
-  let allAnswered = true;
-  
-  for (let i = 0; i < allOptions.length; i++) {
-    if (allOptions[i].getElementsByTagName('button')[0].disabled === false) {
-      allAnswered = false;
-      break;
+    // Passer au formulaire suivant
+    currentForm += 1;
+
+    // Mettre à jour le numéro de la question
+    if (document.getElementById(`QT-${currentForm}`)) {
+        document.getElementById(`QT-${currentForm}`).innerText = `Question ${currentForm}/3`;
     }
-  }
-  
-  if (allAnswered) {
-    showNextForm();
-  }
-}
+    // Gestion de la Barre de progression
+    let elem = document.getElementById("progress-bar"); 
+    if (width <= 49.98) {
+        width += 16.66; // Augmente la barre de progression de 33.33%
+        elem.style.width = width + '%';
+    }
 
-function showNextForm() {
-  document.getElementById(`form-${currentForm}`).style.display = 'none'; // masque le formulaire actuel
-  
-  if (currentForm === totalForms) {
-    showResults();
-  } else {
-    currentForm++;
-    updateProgressBar();
-    document.getElementById(`form-${currentForm}`).style.display = 'block'; // affiche le formulaire suivant
-  }
-}
+    // Afficher le formulaire suivant s'il existe
+    if (document.getElementById(`form-${currentForm}`)) {
+        document.getElementById(`form-${currentForm}`).classList.remove('hidden');
+    }
 
-function updateProgressBar() {
-  const progressBar = document.getElementById('progress-bar');
-  const percentage = (currentForm / totalForms) * 100;
-  progressBar.style.width = percentage + '%';
-}
-
-function showResults() {
-  document.getElementById('form-3').style.display = 'none'; // masque le dernier formulaire
-  document.getElementById('results').style.display = 'block'; // affiche les résultats
-  
-  // Affiche le score final
-  const scoreDisplay = document.getElementById('score');
-  scoreDisplay.textContent = `Votre score final est de ${score} sur ${totalForms}.`;
-}
+    // Afficher le GIF de chargement
+    if (currentForm > 3) {
+        // J'enlève la barre de progréssion
+        let progressBar = document.getElementById('progress-bar');
+        progressBar.remove();
+        // J'enlève le conteneur de la barre de progréssion
+        let test = document.getElementById('hidden-container');
+        test.remove();
+        let loadingGif = document.getElementById('loading-gif');
+        loadingGif.classList.remove('hidden-gif');
+        
+        // Redirige vers la page de résultats après un délai
+        setTimeout(function() {
+            window.location.href = "resultats.html";
+        }, 3000); // Durée du GIF en millisecondes (3secondes)
+    }
+};
